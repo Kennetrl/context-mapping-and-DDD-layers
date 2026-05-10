4b151ddb630d92161ada1b329cdc33e8f310b056
wfandrangop <wfandrangop@uce.edu.ec>
2026-05-10 14:18:32 -0500

    Fix the imports

--- backend/src/main/java/com/veritrabajo/backend/workerprofile/infrastructure/web/WorkerProfileController.java
+++ backend/src/main/java/com/veritrabajo/backend/workerprofile/infrastructure/web/WorkerProfileController.java
@@ -10,6 +10,7 @@ import com.veritrabajo.backend.workerprofile.domain.port.AuthenticatedIdentityPr
 import com.veritrabajo.backend.workerprofile.domain.port.ReputationProvider;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
