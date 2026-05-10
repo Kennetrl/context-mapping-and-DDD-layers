b6ee5161a7696d8bf6f098b980bace031dfb795e
wfandrangop <wfandrangop@uce.edu.ec>
2026-05-10 14:18:32 -0500

    Fix the imports

--- backend/src/main/java/com/veritrabajo/backend/identityaccess/infrastructure/web/AuthController.java
+++ backend/src/main/java/com/veritrabajo/backend/identityaccess/infrastructure/web/AuthController.java
@@ -6,6 +6,7 @@ import com.veritrabajo.backend.identityaccess.application.dto.LoginRequest;
 import com.veritrabajo.backend.identityaccess.application.dto.RegisterAuthRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
